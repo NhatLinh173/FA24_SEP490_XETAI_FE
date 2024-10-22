8c63cabf1f71a561eab3e727f1dc71cd06a5c1e7
Hercules <macbook@Hercules.local>
2024-10-23 00:41:04 +0700

    Update UI Blog

--- src/component/Common/Blog/BlogCard.js
+++ src/component/Common/Blog/BlogCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 // Blog Card 
 const BlogCard = props => {
@@ -8,7 +8,7 @@ const BlogCard = props => {
             <div className="blog_wrapper zoom">
                 <div className="news-img">
                     <Link to="/blog_details">
-                        <img src={props.img} alt="img "/>
+                        <img src={props.img} alt="img " />
                     </Link>
                     <div className="date">
                         <small>{props.month}</small><span>{props.day}</span>
@@ -17,10 +17,6 @@ const BlogCard = props => {
                 <div className="blog_text">
                     <h6><i className="far fa-clock"></i>{props.date}</h6>
                     <h5><Link to="/blog_details">{props.heading}</Link></h5>
-                    <p>{props.para}</p>
-                    <Link to="/blog_details" className="author_name"><i className="far fa-user"></i>{props.name} </Link>
-                    <Link to="/blog_details" className="blog_comments"><i
-                        className="far fa-comment">{props.comment}</i></Link>
                 </div>
             </div>
         </>
